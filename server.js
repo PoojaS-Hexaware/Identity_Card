3672441f7c05de429ada5195b24cb729c671375d
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-09 15:59:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
 var app = express();
 
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.get('/', function (req, res) {
+ global.speech = req.outputContexts.parameters.any;
  res.send(JSON.stringify({ Hello: 'World'}));
 });
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
+
+
 
 
 app.post("/CreateIdCard", function(req, res) {
@@ -24,7 +28,7 @@ app.post("/CreateIdCard", function(req, res) {
         : "Seems like some problem. Speak again.";*/
     return res.json({
       speech: "Id card request submitted successfully!",
-      displayText: speech,
-      source: "webhook-echo-sample"
+      displayText: "Id card request submitted successfully!",
+      source: "webhook"
     });
   });
