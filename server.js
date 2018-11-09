188edb0bf7156bc133b3f2464f633b6bd0bb4d1b
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-09 15:14:35 +0530

    Add files via upload
    updated index file

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
@@ -8,3 +7,18 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
+
+
+app.post("/CreateIdCard", function(req, res) {
+    global.speech =
+      req.body.result &&
+      req.body.result.parameters &&
+      req.body.result.parameters.any
+        ? req.body.result.parameters.any
+        : "Seems like some problem. Speak again.";
+    return res.json({
+      speech: speech,
+      displayText: speech,
+      source: "webhook-echo-sample"
+    });
+  });
