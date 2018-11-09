228f858d5f1a4abf769c8d0a2b4a46155069503c
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-09 18:48:11 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -31,37 +31,35 @@ app.post("/CreateIdCard", function(req, res) {
         ? req.body.result.parameters.any
         : "Seems like some problem. Speak again.";*/
 
-    if (req.body.result.parameters.any) {
-      if (req.body.result.parameters.email) {
-        if (req.body.result.parameters.number) {
-          if (req.body.result.parameters.Designation) {
-            return res.json({
-              speech: req.body.result.parameters.any,
-              displayText: "Id successfully submitted!!",
-              source: "webhook"
-            });
-          } else {
-            return res.json({
-              speech: "Give your Designation" ,
-              displayText: "Give your Designation" , 
-              source: "webhook"
-            });
-          }
-        } else {
-          return res.json({
-            speech: "Give your phone number" ,
-            displayText: "Give your phone number" , 
-            source: "webhook"
-          });
-        }
-      } else {
-        return res.json({
-          speech: "Give your email Id" ,
-          displayText: "Give your email Id" , 
-          source: "webhook"
-        });
-      } 
-    } 
+
+    if (req.body.result.parameters.any && req.body.result.parameters.email
+      && req.body.result.parameters.number && req.body.result.parameters.Designation ) {
+      return res.json({
+        speech: req.body.result.parameters.any,
+        displayText: "Id successfully submitted!!",
+        source: "webhook"
+      });
+    } else if (req.body.result.parameters.any && !req.body.result.parameters.email) {
+      return res.json({
+        speech: "Give your email Id" ,
+        displayText: "Give your email Id" , 
+        source: "webhook"
+      });
+    } else if (req.body.result.parameters.any && req.body.result.parameters.email
+      && !req.body.result.parameters.number) {
+      return res.json({
+        speech: "Give your phone number" ,
+        displayText: "Give your phone number" , 
+        source: "webhook"
+      });
+    } else if (req.body.result.parameters.any && req.body.result.parameters.email
+      && req.body.result.parameters.number && !req.body.result.parameters.Designation ) {
+      return res.json({
+        speech: "Give your Designation" ,
+        displayText: "Give your Designation" , 
+        source: "webhook"
+      });
+    }
   });
  
     /*if (req.body.result.parameters.any) {
