32329800c1d54c0c0f058051ea42fa04c8edf24a
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-12 02:00:44 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -24,39 +24,19 @@ app.listen(port, function () {
 
 
 app.post("/demo", function(req, res) {
-  /*if (req.body.result.parameters.any && req.body.result.parameters.email
-    && req.body.result.parameters.number && req.body.result.parameters.Designation ) {
-    return res.json({
-      speech: req.body.result.parameters.any,
-      displayText: "Id successfully submitted!!",
-      source: "webhook"
-    });
-  } else if ( req.body.result.parameters.email == 'null') {
-    return res.json({
-      speech: "Give your email Id" ,
-      displayText: "Give your email Id" , 
-      source: "webhook"
-    });
-  } else if (req.body.result.parameters.number == 'null') {
-    return res.json({
-      speech: "Give your phone number" ,
-      displayText: "Give your phone number" , 
-      source: "webhook"
-    });
-  } else if (req.body.result.parameters.Designation == 'null') {
-    return res.json({
-      speech: "Give your Designation" ,
-      displayText: "Give your Designation" , 
-      source: "webhook"
-    });
-  }*/
     /*global.speech =
       req.body.result &&
       req.body.result.parameters &&
       req.body.result.parameters.any
         ? req.body.result.parameters.any
         : "Seems like some problem. Speak again.";*/
-      if (req.body.result.parameters.any) {
+      if (!req.body.result.parameters.any) {
+          res.json({
+            speech: "Give your name",
+            displayText: "Give your name", 
+            source: "webhook"
+          })
+        
         if (!req.body.result.parameters.email) {
           res.json({
             speech: "Give your email Id",
@@ -80,12 +60,12 @@ app.post("/demo", function(req, res) {
               source: "webhook"
             })
           }
-          return res.json({
-            speech: req.body.result.parameters.any,
-            displayText: "Id successfully submitted!!",
-            source: "webhook"
-          });
-        } 
+        }
+        return res.json({
+          speech: req.body.result.parameters.any,
+          displayText: "Id successfully submitted!!",
+          source: "webhook"
+        });
     });
 
    
