ff49cef4959c1715be1adcf61f4e613a6cbe65b2
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-12 02:05:12 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -60,12 +60,14 @@ app.post("/demo", function(req, res) {
               source: "webhook"
             })
           }
+          if (req.body.result.Designation) {
+            return res.json({
+              speech: req.body.result.parameters.any,
+              displayText: "Id successfully submitted!!",
+              source: "webhook"
+            });
+          }
         }
-        return res.json({
-          speech: req.body.result.parameters.any,
-          displayText: "Id successfully submitted!!",
-          source: "webhook"
-        });
     });
 
    
