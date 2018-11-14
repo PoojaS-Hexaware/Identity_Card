8236878e1589763a06bb880faf2dba9524bc013b
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-14 19:24:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -85,9 +85,9 @@ app.post("/demo", function(req, res) {
                           "basicCard": {
                               "title": "Identity Card",
                               "formattedText": "**Name** : " + id.name +
-                                "  **Phone Number** : " + id.number +
-                                "  **Email ID** : " + id.email +
-                                "  **Designation** : " + id.designation,
+                                "\n  **Phone Number** : " + id.number +
+                                "\n  **Email ID** : " + id.email +
+                                "\n  **Designation** : " + id.designation,
                               "buttons" : []
                           }
                         }
