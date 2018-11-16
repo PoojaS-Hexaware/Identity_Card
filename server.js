d308ecdf4cdefdb129491fcdedc7450548d41edc
PoojaS2-Hexaware <44828853+PoojaS2-Hexaware@users.noreply.github.com>
2018-11-16 20:28:27 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -296,16 +296,16 @@ app.post("/demo", function(req, res) {
     if(req.body.result.metadata.intentName == 'ViewRequests - custom') {
       keySelected = req.body.result.parameters.number;
       
-      var selectedId = {
+      /*var selectedId = {
         'name'  : null,
         'email' : null,
         'number' : null,
         'designation' : null,
-      };
+      };*/
       for (i=0 ; i < requestCard.length; i++) {
         //selectedId.number = requestCard[i].number
         if (keySelected == requestCard[i].number) {
-          selectedId = {
+          global.selectedId = {
             'name'  : requestCard[i].name,
             'email' : requestCard[i].email,
             'number' : requestCard[i].number,
